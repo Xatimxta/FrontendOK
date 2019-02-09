ea079942e3ea376c37f6a09a33f8786f4a787eac
Renzo <renzo1805@hotmail.com>
2019-02-09 02:16:29 +0100

    Agregado drops y upcoming OK

---
+++ src/app/drops/drops.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector:'drops-tag',
+    templateUrl:'./drops.component.html',
+    styleUrls:['./drops.component.css']    
+
+})
+
+export class DropsComponent{
+    news={
+        title:"",
+        words:""
+    };
+
+};
