0a0306bb3763a948bb13d81e31508415ee37031f
Renzo <renzo1805@hotmail.com>
2019-04-26 11:47:10 +0200

    "Cambios en imagenes , borrado prueba"

--- src/app/drops/drop.ts
+++ src/app/drops/drop.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class Drop{
     idSNEAKER:number;
     brand:string="";
@@ -15,22 +14,4 @@ export class Drop{
         this.dropDate=dropDate;
         this.retailPrice=retailPrice;
     }
-=======
-export class Drop{
-    idSNEAKER:number;
-    brand:string="";
-    model:string="";
-    colorWay:string="";
-    dropDate:Date;
-    retailPrice:number;
-    constructor( idSNEAKER:number, brand:string="", model:string="",
-    colorWay:string="",dropDate:Date, retailPrice:number){
-        this.idSNEAKER=idSNEAKER;
-        this.brand=brand;
-        this.model=model;
-        this.colorWay=colorWay;
-        this.dropDate=dropDate;
-        this.retailPrice=retailPrice;
-    }
->>>>>>> e24cd8adcb9b5be1e00c4294b24b3a1a7cc3da92
 }
