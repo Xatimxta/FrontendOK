f4f9525dadb148ae6f2e7c1404cdfb1bd070f078
Renzo <renzo1805@hotmail.com>
2019-05-09 11:29:54 +0200

    "Editnews"

---
+++ src/app/editnews/editnews.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { PostService } from '../news/news.service';
+import { Post } from '../news/post';
+
+@Component({
+
+    selector:'addnews-tag',
+    templateUrl:'./editnews.component.html',
+    styleUrls:['./editnews.component.css'],
+    providers:[PostService]
+
+})
+
+export class AddNewsComponent {
+    news: Post[] = [];
+     date = new Date('2017-03-07T10:00:00');
+     str = this.date.toDateString();
+    //datetime = new Date('Y-m-d');
+    //datetime.toDateString();
+    nuevoPost : Post = new Post(null, "", this.str, 4, "", "" );
+
+    constructor(private serviceUser: PostService) { }
+
+    editThisPost() {
+        this.serviceUser.editNew(this.nuevoPost).
+          subscribe(
+            (result) => {
+              console.log("----")
+              // Devuelve el parametro en JSON del PHP.
+             // this.news = result["resposta"];
+    
+              //console.log(this.news);
+
+              console.log(result);
+            },
+            (error) => { console.log(error) }
+          );
+    
+      }
+
+};
