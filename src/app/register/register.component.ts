016e2196e98b93dab3b0f27480f7e14fce714fcb
Renzo <renzo1805@hotmail.com>
2019-05-15 23:49:53 +0200

    "Modificacion registro"

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Usuario } from './usuario';
 import { UsuarioService } from './usuario.service';
+import { Router, ActivatedRoute, Params } from '@angular/router'; 
 
 
 @Component({
@@ -17,14 +18,19 @@ export class RegisterComponent{
   nuevoUsuario: Usuario = new Usuario(null, "",
     "", new Date(), "", "","");
   
-  constructor(private serviceUser: UsuarioService) { }
+  constructor(private serviceUser: UsuarioService, private _router: Router,
+    private _activRoute: ActivatedRoute) { }
 
   enviaFormulari() {
+    
+    .0
     this.serviceUser.postProducte(this.nuevoUsuario).
       subscribe(
         (result) => {
           console.log("----")
           console.log(result);
+          alert("Registro con éxito\nSerás redirigido a Login");
+          this._router.navigate(['/login']);
         },
         (error) => { console.log(error) }
       );
