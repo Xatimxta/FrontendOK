3e34783c157989f801dfa0a308f01fe203b5a944
Renzo <renzo1805@hotmail.com>
2019-05-27 12:48:31 +0200

    "Cambios en realizacion, pendiente finalizarlos"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
 
@@ -13,5 +14,26 @@ export class HeaderComponent{
         title:"",
         words:""
     };
+    fail:string = "";
+    failproxy: string = "";
+    constructor(private _router: Router) { }
+    checkUser(){
+        var token =  localStorage.getItem('token');
+        if(token == null){
+            this.fail = "No hay usuario logeado"
+        }else{
+            this._router.navigate(['/profile']);
+        }
+    }
+    checkUser2(){
+        var token = localStorage.getItem('token');
+        console.log(token);
+        if(token == null){
+            this.failproxy = "No puedes acceder sin logear"
+        }else{
+            this._router.navigate(['/proxy']);
+        }
+    }
 
+    
 };
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,12 +5,13 @@ import * as user from 'src/app/login/login.component';
 import { getAllDebugNodes } from "@angular/core/src/debug/debug_node";
 import { Comment } from "./Comment";
 import { Router, ActivatedRoute, Params } from '@angular/router'; 
-import * as jwt_decode from "jwt-decode";
+
 export var superidComment: string = '';
 export var superidUser: string = '';
 export var superidPost: string = '';
 export var supercomment: string = '';
-export var superdate: string = '';
+//export var superdate: string = '';
+
 @Component({
 
     selector:'profile-tag',
@@ -22,58 +23,108 @@ export var superdate: string = '';
 
 export class ProfileComponent  {
     profile: Profile[] = [];
+    
     //foto: string;
     constructor(private serviceProfile: ProfileService,private _router: Router,
         private _activRoute: ActivatedRoute) { }
-    comment: Comment[] = [];
-    id = user.superid;
-    name = user.supername;
-    surname = user.supersurname;
-    username = user.supername;
-    birthdate = user.superbirthdate;
-    address = user.superaddress;
-    email = user.superemail;
-    password = user.superpass;
-    foto = user.superphoto;
+    //comment: Comment[] = [];
+        superdate: string = '';
+        id;
+        name;
+        surname;
+        birthdate;
+        username;
+        address;
+        email;
+        password;
+        foto;
+        date;
+        logout = false;
+        
+        
+    
     ngOnInit(): void {
-        this.getLogin(this.id);
+        var a =  localStorage.getItem('token');
+        console.log("toKen: "+ a);
+        if(a == null){
+            console.log("toKen1: "+ a);
+            this.id = "";
+            this.name = "";
+            this.surname = "";
+            this.username = "";
+            this.birthdate = "";
+            this.address = "";
+            this.email = "";
+            this.password = "";
+            this.foto = "";
+            this.date = "";
+            
+        }else if(a != null){
+            console.log("toKen2: "+ a);
+            this.id = user.superid;
+            this.name = user.supername;
+            this.surname = user.supersurname;
+            this.username = user.supername;
+            this.birthdate = user.superbirthdate;
+            this.address = user.superaddress;
+            this.email = user.superemail;
+            this.password = user.superpass;
+            this.foto = user.superphoto;
+            this.date = this.superdate;
+        }
+        
     }
-    getLogin(id){
+    logOut(){
+       console.log("log Out");
+       console.log("token in: " + localStorage.getItem('token'));
+            localStorage.removeItem('token');
+            console.log("token out: " + localStorage.getItem('token'));
+            this.id = "";
+            this.name = "";
+            this.surname = "";
+            this.username = "";
+            this.birthdate = "";
+            this.address = "";
+            this.email = "";
+            this.password = "";
+            this.foto = "";
+            this.date = "";
+            this._router.navigate(['/login']);
+        /*let logout:boolean = true;
+        //console.log(usu);
+        this.serviceProfile.logOut( logout ).subscribe((result) => {
+            
+                console.log("Token OK");
+                localStorage.setItem("token",result);
+                console.log(result);
+                 //redirect
+                 this.getDecodedAccessToken(result);
+                //this._router.navigate(['/profile']);
+            
+        } , (error) => {
+            console.log(error);
+        });*/
+    }
+    /*getLogin(id){
         //console.log("GET LOGIN!")
         let comment = new Comment(user.superid);
         //console.log(usu);
         this.serviceProfile.getPost( comment ).subscribe((result) => {
-            if(result.token !=null){
+            
                 console.log("Token OK");
-                localStorage.setItem("token",result.token);
-                console.log(result.token);
+                localStorage.setItem("token",result);
+                console.log(result);
                  //redirect
-                 this.getDecodedAccessToken(result.token);
+                 this.getDecodedAccessToken(result);
                  console.log("Id Comment: " + superidComment);
                  console.log("id User: " + superidUser);
                  console.log("id post: " + superidPost);
                  console.log("comment: " + supercomment);
-                 console.log("date: " + superdate);
+                 console.log("date: " + this.superdate);
                 //this._router.navigate(['/profile']);
-            }else{
-                console.log("Comment FAIL");
-            }
+            
         } , (error) => {
             console.log(error);
         });
-    }
-    getDecodedAccessToken(token: string): any {
-        try{
-            var data = jwt_decode(token);
-            superidComment = data["idComment"];
-            superidUser = data["idUser"];
-            superidPost = data["idPost"];
-            supercomment = data["comment"];
-            superdate = data["date"];
-            return data;
-        }
-        catch(Error){
-            return null;
-        }
-      }
+    }*/
 };
