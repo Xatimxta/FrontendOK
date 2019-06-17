f6b1349f3aa43dd802164e9f307e35111383e6f8
Mario Tamargo Sanz <36266348+MarioTamargo@users.noreply.github.com>
2019-06-17 21:14:00 +0200

    Update news.component.ts

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -5,44 +5,58 @@ import { Router } from "@angular/router";
 
 @Component({
 
-    selector:'news-tag',
-    templateUrl:'./news.component.html',
-    styleUrls:['./news.component.css'],
-    providers:[PostService]
+  selector: 'news-tag',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css'],
+  providers: [PostService]
 
 })
 
 export class NewsComponent implements OnInit {
-    news: Post[] = [];
-    failproxy: string = "";
+  news: Post[] = [];
+  failproxy: string = "";
+  totalPagesInPaginator: number[];
+  currentPage: number;
+  pageSize: number = 3
+  private dataset: Post[];
 
-    constructor(private serviceNews: PostService, private _router: Router) { }
+  constructor(private serviceNews: PostService, private _router: Router) { }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.getNews();
+  }
 
-        this.getNews();
-      }
+  setCurrentPage(cPage: number) {
+    this.currentPage = cPage
 
-      getNews() {
-        this.serviceNews.getNews().subscribe(
-          (result) => {
-            console.log(result["data"]);
-            this.news = result["data"];
-    
-          },
-          (error) => {
-            console.log(error);
-          });
-      }
+    this.news = [...this.dataset].splice(this.pageSize * cPage, this.pageSize);
+  }
 
-      checkUser3(){
-        var token = localStorage.getItem('token');
-        console.log(token);
-        if(token == null){
-            this.failproxy = "No puedes acceder sin logear"
-        }else{
-            this._router.navigate(['/news']);
-        }
+  getNews() {
+    this.serviceNews.getNews().subscribe(
+      (result) => {
+        this.dataset = result["data"]
+
+        this.currentPage = 0
+        let totalShoes = this.dataset.length
+
+        this.totalPagesInPaginator = Array(Math.ceil(totalShoes / this.pageSize)).fill(0).map((x, i) => i);
+
+        this.news = [...this.dataset].splice(0, this.pageSize);
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
+
+  checkUser3() {
+    var token = localStorage.getItem('token');
+    console.log(token);
+    if (token == null) {
+      this.failproxy = "No puedes acceder sin logear"
+    } else {
+      this._router.navigate(['/news']);
     }
+  }
 
 };
